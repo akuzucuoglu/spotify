8dfd3579ebd5a127dca2f96b7f3632a37d273224
Ahmet Kuzucuoglu <ahmetkuzucuoglu@gmail.com>
2018-08-12 17:41:28 +0300

    audio analysis

---
+++ Spotify/simple_spotify_cnn_audio_analysis.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 12 17:16:44 2018
+
+@author: Ahmet
+"""
+
+
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import spotipy.util as util
+import requests
+
+import pandas as pd
+import IPython.display as ipd
+import librosa
+import librosa.display
+import glob 
+import matplotlib.pyplot as plt
+
+#1. Authentication Part
+cid =input("Please enter your client id here: ")
+secret = input("Please enter your secret here: ")
+username = input("Please enter your username here: ")
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret) 
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+#scope = 'user-library-read playlist-read-private'
+scope = 'playlist-modify-public '
+scope += 'user-library-read '
+scope += 'user-follow-read '
+scope += 'user-library-modify '
+scope += 'user-read-private '
+scope += 'user-top-read'
+
+token = util.prompt_for_user_token(username, scope,cid,secret,redirect_uri='http://localhost:8888')
+
+sp = spotipy.Spotify(auth=token)
+
+#bunu baştan initialize ediyorum.
+SavedSongURLs=[]
+v_length=sp.current_user_saved_tracks()["total"]
+
+
+#Defining downloading function
+def download_file(url,filename):
+    #''.join(fix for fix in filename if fix.isalnum())
+    local_filename = ''.join(fix for fix in filename if fix.isalnum()) +'.wav' #url.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024): 
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+                #f.flush() commented by recommendation from J.F.Sebastian
+    return local_filename
+
+
+#Getting the previews for the songs liked
+os.chdir('C:\\Users\\Ahmet\\Desktop\\machine learning\\Convolutional_Neural_Networks\\audio_dataset\\liked2')
+songNum=0
+audio_data=[]
+for v_offset in range(0,v_length+20,20):
+    print(v_offset)
+    SavedTracks=sp.current_user_saved_tracks(limit=20,offset=v_offset)
+    SavedSongs=SavedTracks["items"]
+    for song in SavedSongs:
+        if song['track']['preview_url'] is None:
+            None
+        else:
+            songNum=songNum+1
+            SavedSongURLs.append(song['track']['preview_url'])
+            download_file(song['track']['preview_url'],'song'+(str(songNum)))
+            audio_data.append(librosa.load('C:\\Users/Ahmet/Desktop/machine learning/Convolutional_Neural_Networks/audio_dataset/liked2/song'+str(songNum)+'.wav'))
+
+
+#Getting the songs and their features from my DislikedSongs playlist
+#Not much different than the upper
+myDislikePl= sp.user_playlist(user=username, playlist_id='0ij13jfgJQg3rvyA6T19hP?')
+
+os.chdir('C:\\Users\\Ahmet\\Desktop\\machine learning\\Convolutional_Neural_Networks\\audio_dataset\\disliked2')
+DislikedSongsURLs=[]
+v_length2=myDislikePl["tracks"]["total"]
+for v_dlTrack in myDislikePl["tracks"]["items"]:
+    print(v_dlTrack["track"]["preview_url"])
+    if v_dlTrack['track']['preview_url'] is None:
+            None
+    else:
+            songNum=songNum+1
+            DislikedSongsURLs.append(v_dlTrack["track"]["preview_url"])
+            download_file(v_dlTrack['track']['preview_url'],'song'+(str(songNum)))
+            audio_data.append(librosa.load('C:\\Users/Ahmet/Desktop/machine learning/Convolutional_Neural_Networks/audio_dataset/disliked2/song'+str(songNum)+'.wav'))
+
+
+
+
+ = librosa.load('C:\\Users/Ahmet/Desktop/machine learning/Convolutional_Neural_Networks/audio_dataset/liked2/song'+str(songNum)+'.wav')
+      
+#librosa audio object
+ipd.Audio('C:\\Users/Ahmet/Desktop/machine learning/Convolutional_Neural_Networks/audio_dataset/liked/24HoursIstanbul.wav')
+
+#getting the spectogram out of librosa audio file 
+data, sampling_rate = librosa.load('C:\\Users/Ahmet/Desktop/machine learning/Convolutional_Neural_Networks/audio_dataset/liked/24HoursIstanbul.wav')
+
+#drawing the spectogram
+plt.figure(figsize=(20, 4))
+librosa.display.waveplot(data, sr=sampling_rate)
